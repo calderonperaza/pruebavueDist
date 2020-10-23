8371168a38cf86a4b9160816ba0d13294c38fc1e
Ernesto Alexander Calderon Peraza <calderonperaza@gmail.com>
2020-10-22 21:35:51 -0600

    otro intento

---
+++ src/components/Botoncuenta.vue
@@ -0,0 +1,24 @@
+<template>
+<button @click="lanzarEvento">{{titulo}}</button>
+</template>
+
+<script>
+export default {
+    name: "Botoncuenta",
+    props: {
+        titulo: {
+            type: String,
+            default: "Soy un boton"
+        }
+    },
+    methods: {
+        lanzarEvento() {
+            this.$emit("hicieronclick", 25)
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Cuenta.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    <h1>Mi cuenta de ahorros</h1>
+    <h2> $ {{ ahorro}}</h2>
+    <h3>Gastos de la semana</h3>
+    <p v-for="(item,index) in gastos" :key="index">
+        {{ item }}
+    </p>
+    <Botoncuenta titulo="Disminuir" @hicieronclick="DisminuirAhorro" :disabled="ahorro===0" />
+    <Botoncuenta titulo="Aumentar" @hicieronclick="AumentarAhorro" />
+
+</div>
+</template>
+
+<script>
+import Botoncuenta from "./Botoncuenta"
+
+export default {
+    name: "Cuenta",
+    components: {
+        Botoncuenta
+    },
+    data() {
+        return {
+            ahorro: 100,
+            gastos: ["comprar comida", "libros", "Electricidad", "agua"]
+        }
+    },
+    methods: {
+        AumentarAhorro(valor) {
+            this.ahorro += valor;
+        },
+        DisminuirAhorro(valor) {
+            this.ahorro -= valor;
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
